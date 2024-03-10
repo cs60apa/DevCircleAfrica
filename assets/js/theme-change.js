d16ccaf1939dbee6768f61d090ccba88a0d3a5d0
JiriDavid <jiridavidpromise@gmail.com>
2024-03-10 22:45:16 +0530

    fixed brocken links, conlficts, removed in styles

--- assets/js/theme-change.js
+++ assets/js/theme-change.js
@@ -3,7 +3,7 @@ const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-  
+
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
     }
@@ -16,7 +16,7 @@ function switchTheme(e) {
     }
     else {        document.documentElement.setAttribute('data-theme', 'light');
           localStorage.setItem('theme', 'light');
-    }    
+    }
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
